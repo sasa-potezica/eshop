d5d6d13e5b0ba5c821e4d6cf5be3a02c7279ecff
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-03-24 08:10:54 +0100

    Implement Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,75 @@
 import React from "react";
+import { IoMdSearch } from "react-icons/io";
 
+const MenuLinks = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Shop",
+    link: "/#shop",
+  },
+  {
+    id: 3,
+    name: "About",
+    link: "/#about",
+  },
+  {
+    id: 4,
+    name: "Blogs",
+    link: "/#blog",
+  },
+];
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
+      <div className="py-4">
+        <div className="container flex justify-between items-center">
+          {/* Logo and Link section */}
+          <div className="flex items-center gap-4">
+            <a
+              href="#"
+              className="text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl"
+            >
+              Eshop
+            </a>
+            {/* Menu Items */}
+            <div className="hidden lg:block">
+              <ul className="flex items-center gap-4">
+                {MenuLinks.map((data, index) => (
+                  <li key={index}>
+                    <a
+                      href={data.link}
+                      className="inline-block px-4 font-semibold text-gray-600 hover:text-black dark:hover:text-white duration-200"
+                    >
+                      {data.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          {/* Navbar Right section */}
+          <div className="flex justify-between items-center gap-4">
+            {/* Search Bar section */}
+            <div className="relative group hidden sm:block">
+              <input
+                type="text"
+                placeholder="Search"
+                className="search-bar pl-1"
+              />
+              <IoMdSearch className="text-xl text-gray-600 group-hover:text-primary dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3 duration-200" />
+            </div>
+            {/* Dark Mode section */}
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
