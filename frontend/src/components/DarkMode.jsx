77d432edd00806653fb00d9413da33f9ef55e027
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-03-28 09:12:10 +0100

    Add Dark Mode Feature

--- frontend/src/components/DarkMode.jsx
+++ frontend/src/components/DarkMode.jsx
@@ -1,11 +1,41 @@
-import React, { useState } from 'react'
-import LightButton from '../assets/website/light-mode-button.png';
-import DarkButton from '../assets/website/dark-mode-button.png';
+import React, { useEffect, useState } from "react";
+import LightButton from "../assets/website/light-mode-button.png";
+import DarkButton from "../assets/website/dark-mode-button.png";
 const DarkMode = () => {
-    const [theme, setTheme]=useState('light');
-  return (
-    <div><img src={LightButton} alt="" className={`w-12 cursor-pointer `} /></div>
-  )
-}
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
 
-export default DarkMode
+  useEffect(() => {
+    const element = document.documentElement;
+    localStorage.setItem("theme", theme);
+    if (theme === "dark") {
+      element.classList.add("dark");
+      element.classList.add("dark");
+    } else {
+      element.classList.remove("light");
+      element.classList.remove("dark");
+    }
+  });
+
+  return (
+    <div className="relative">
+      <img
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        src={LightButton}
+        alt=""
+        className={`w-12 cursor-pointer absolute right-0 z-10 ${
+          theme === "dark" ? "opacity-0" : "opacity-100"
+        } transition-all duration-300`}
+      />
+      <img
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        src={DarkButton}
+        alt=""
+        className={`w-12 cursor-pointer`}
+      />
+    </div>
+  );
+};
+
+export default DarkMode;
