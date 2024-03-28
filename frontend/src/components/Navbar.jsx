924d0662239118e66ee1e5a30446ed2fd29ceecb
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-03-28 09:50:31 +0100

    Implement Dropdown Menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { IoMdSearch } from "react-icons/io";
 import DarkMode from "./DarkMode";
-
+import { FaCaretDown } from "react-icons/fa";
 const MenuLinks = [
   {
     id: 1,
@@ -22,6 +22,23 @@ const MenuLinks = [
     id: 4,
     name: "Blogs",
     link: "/#blog",
+  },
+];
+const DropdownLinks = [
+  {
+    id: 1,
+    name: "Trending Products",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Best Selling",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Top Rated",
+    link: "/#",
   },
 ];
 const Navbar = () => {
@@ -50,6 +67,31 @@ const Navbar = () => {
                     </a>
                   </li>
                 ))}
+                <li className="relative cursor-pointer group">
+                  <a
+                    href="#"
+                    className="flex items-center gap-[2px] font-semibold text-gray-500 dark:hover:text-white py-2"
+                  >
+                    Quick Links
+                    <span>
+                      <FaCaretDown className="group-hover:rotate-180 duration-300" />
+                    </span>
+                  </a>
+                  <div className="absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white shadow-md dark:bg-gray-900 p-2 dark:text-white">
+                    <ul className="space-y-2">
+                      {DropdownLinks.map((data, index) => (
+                        <li>
+                          <a
+                            className="text-gray-500 dark:hover:text-white duration-200 inline-block w-full p-2 hover:bg-primary/20 rounded-md font-semibold"
+                            href={data.link}
+                          >
+                            {data.name}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </li>
               </ul>
             </div>
           </div>
